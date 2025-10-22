dc45c72a3f2e88981a88344ed2e74fa5a58a422c
Horror Face <raphaelds305@gmail.com>
2025-10-22 08:01:34 -0400

    Update Cours8.java

--- Cours8/Cours8.java
+++ Cours8/Cours8.java
@@ -7,6 +7,7 @@ public class Cours8 {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int[] tableau1 = { 4, 3, 22, 110, 264, 16};
+        //index tableau =  0 /1  /2   /3   /4  /5
 
         System.out.println("Tableau droit: ");
         for(int i = 0;i < tableau1.length;i++){
@@ -17,6 +18,12 @@ public class Cours8 {
         for(int i = tableau1.length -1; i >= 0; i--){
             System.out.println(tableau1[i]);
         }
+
+
+        int[] tableau2 = new int[2];
+        tableau2[0] = 100;
+        tableau2[1] = 10;        
+        tableau2[2] = 20;
         sc.close();
     }
 }
